896dfbb66a979c74be56c8800d83fa8dee563b71
ErnestoCapambo <ernestosikilitacapambo@gmail.com>
2025-03-09 09:39:18 +0100

    Settiing model fields as nullable

--- src/controllers/model-controllers/CreateModelFreelanceController.ts
+++ src/controllers/model-controllers/CreateModelFreelanceController.ts
@@ -4,5 +4,6 @@ import { Request, Response } from "express";
 export class CreateModelFreelanceController {
     async handle(request: Request, response: Response) {
         const { userId } = request.params
+        
     }
 }
